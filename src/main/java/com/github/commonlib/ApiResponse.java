b5bb92a37ddec190769f4032f15b6ca866624d5f
oeunz <oeunzzang@naver.com>
2024-10-25 19:35:35 +0900

    feat: common response 생성

---
+++ src/main/java/com/github/commonlib/ApiResponse.java
@@ -0,0 +1,44 @@
+package com.github.commonlib;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApiResponse<T> {
+
+    private static final String STATUS_SUCCESS = "success";
+    private static final String STATUS_FAIL = "fail";
+    private static final String STATUS_ERROR = "error";
+
+    private String status;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    private ApiResponse(String status, T data, String message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(STATUS_SUCCESS, data, null);
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(STATUS_SUCCESS, data, message);
+    }
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(STATUS_FAIL, null, message);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(STATUS_ERROR, null, message);
+    }
+}
