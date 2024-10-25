ee18133cbf875363a11359ef9e2aa971d7ab0561
oeun42 <oeunzzang@naver.com>
2024-10-25 15:52:23 +0900

    initialize commonlib

---
+++ src/main/java/com/github/commonlib/CommonlibApplication.java
@@ -0,0 +1,13 @@
+package com.github.commonlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonlibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonlibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/commonlib/CommonlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.commonlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonlibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
